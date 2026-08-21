50d178f785c031e11fb1cd989cc4ff0126616cf8
Alexey Maltsev <alexey.maltsev@itrexgroup.com>
2026-08-21 15:11:47 +0300

    chore: rename package from @wdk/core to dev-websites-core
    Align the publishable package name with the repo (dev-websites-core) instead
    of the interim @wdk/core scope.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/** Prefer domain imports: `@wdk/core/cms`, `@wdk/core/seo`, etc. */
+/** Prefer domain imports: `dev-websites-core/cms`, `dev-websites-core/seo`, etc. */
 export * from './cms/index.js'
 export * from './seo/index.js'
 export * from './i18n/index.js'
